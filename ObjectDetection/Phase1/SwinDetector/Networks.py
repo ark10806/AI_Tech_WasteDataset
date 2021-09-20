0680b41a6cedc0115781e0c7891ab0e6799e2fa3
root <ark10806@gmail.com>
2021-09-20 17:17:59 +0000

    todo: train.py

--- ObjectDetection/Phase1/SwinDetector/Networks.py
+++ ObjectDetection/Phase1/SwinDetector/Networks.py
@@ -2,7 +2,7 @@ from models.swin_transformer import SwinTransformer
 import torch.nn as nn
 
 class SwinTransformerObjectDetection(nn.Module):
-    def __init__(self, n_classes):
+    def __init__(self, n_classes=60):
         super(SwinTransformerObjectDetection, self).__init__()
         self.backbone = SwinTransformer()
         in_features = self.backbone.head.in_features
